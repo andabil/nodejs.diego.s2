29dde3477d68508106651a92279323bc3d1938ff
andabil <andamon.abilar@avensia.com>
2018-09-10 16:58:59 +0800

    this is the commit

--- index.ts
+++ index.ts
@@ -3,6 +3,15 @@ import command from './command';
 async function init() {
   try {
     await command('git fetch --all');
+    const {stdout: branchName} = await command ('git rev-parse --abbrev-ref HEAD');
+    await command('git add .');
+    await command('git commit -m "this is the commit" ');
+
+    await command('git checkout develop');
+    await command('git pull --rebase');
+    await command(`git checkout ${branchName}`);
+    await command('git rebase develop');
+    
   } catch(e) {
     console.error('An error baby: ', e);
   }
